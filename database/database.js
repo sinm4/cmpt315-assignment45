c8f8890c485ed4b48e02c193a25c4b7979b8c7b1
sinm4 <sinm4@mymacewan.ca>
2024-02-14 19:04:21 -0700

    Revert to original

--- database/database.js
+++ database/database.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 
 export const connectDB = async () => {
-  const url =
-    "mongodb+srv://sinm4:8iOcBhMVVXWp0Z5Q@cluster0.2xmtyoe.mongodb.net/?retryWrites=true&w=majority";
+  const url = `mongodb://localhost:27017/cmpt315assignment45`;
 
   try {
     const connection = await mongoose.connect(url, {
