338adb6cb3a9bd58c973cd32086dbcc1145426c9
sinm4 <sinm4@mymacewan.ca>
2024-02-14 19:00:04 -0700

    Change url

--- database/database.js
+++ database/database.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", true);
 
 export const connectDB = async () => {
   const url =
-    "mongodb+srv://sinm4:zRfeyBnBMVsQ5GID@cluster0.2xmtyoe.mongodb.net/";
+    "mongodb+srv://sinm4:8iOcBhMVVXWp0Z5Q@cluster0.2xmtyoe.mongodb.net/?retryWrites=true&w=majority";
 
   try {
     const connection = await mongoose.connect(url, {
